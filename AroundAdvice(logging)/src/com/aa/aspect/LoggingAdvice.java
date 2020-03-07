61561f0a84f1e5d2201e400c53fdf4ec90ace98e
Admin <Admin@ADMINRG-PK096SO>
2020-03-07 22:25:37 +0530

    Added crosscutting logic

--- AroundAdvice(logging)/src/com/aa/aspect/LoggingAdvice.java
+++ AroundAdvice(logging)/src/com/aa/aspect/LoggingAdvice.java
@@ -1,5 +1,33 @@
 package com.aa.aspect;
 
-public class LoggingAdvice {
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class LoggingAdvice implements MethodInterceptor{
+
+	@Override
+	public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+		String methodName = null;
+		Object[] args = null;
+		
+		methodName = methodInvocation.getMethod().getName();
+		args = methodInvocation.getArguments();
+		
+		for(int i = 0;i < args.length;i++)
+		{
+			if(i == 0)
+			{
+				System.out.println("Before entering"+methodName+"("+args[i]);
+				continue;
+			}
+			System.out.println(",");
+		}
+		System.out.println(")");
+		
+		Object ret = methodInvocation.proceed();
+		System.out.println("Exit"+ret);
+				
+		return ret;
+	}
 
 }
