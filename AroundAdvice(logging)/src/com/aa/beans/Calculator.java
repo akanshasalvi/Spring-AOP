29129f5861be400c081953fa2980825bae2cf9b8
Admin <Admin@ADMINRG-PK096SO>
2020-03-07 22:07:56 +0530

    Primary logic

---
+++ AroundAdvice(logging)/src/com/aa/beans/Calculator.java
@@ -0,0 +1,12 @@
+package com.aa.beans;
+
+public class Calculator {
+	
+	public int add(int a, int b)
+	{
+		int sum = 0;
+		sum = a + b;
+		System.out.println("Inside add() method");
+		return 0;
+	}
+}
