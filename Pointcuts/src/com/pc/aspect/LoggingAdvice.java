98e93a5f09466816ff90c9fe20b43d3a46e2c42f
Admin <Admin@ADMINRG-PK096SO>
2020-03-13 10:09:43 +0530

    Wrote crosscutting logic

---
+++ Pointcuts/src/com/pc/aspect/LoggingAdvice.java
@@ -0,0 +1,22 @@
+package com.pc.aspect;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.MethodBeforeAdvice;
+
+public class LoggingAdvice implements MethodInterceptor{
+
+	@Override
+	public Object invoke(MethodInvocation methodinvocation) throws Throwable {
+		String methodName = methodinvocation.getMethod().getName();
+		Object[] args = methodinvocation.getArguments();
+		
+		System.out.println("Entering in method"+methodName+"with arguments ("+args[0]+","+args[1]+")");
+		
+		Object ret = methodinvocation.proceed();
+		
+		System.out.println("Exiting from method "+methodName+ "with return value "+ret);
+		return ret;
+	}
+
+}
