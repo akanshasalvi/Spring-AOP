c3258e8c0c7dcb251ad6ce3d75c27da7b29204a1
Admin <Admin@ADMINRG-PK096SO>
2020-03-13 10:01:36 +0530

    Initial commit

---
+++ Pointcuts/src/com/pc/beans/Calculator.java
@@ -0,0 +1,16 @@
+package com.pc.beans;
+
+public class Calculator {
+	public int add(int a, int b)
+	{
+		int sum = 0;
+		sum = a + b;
+		System.out.println("In add()");
+		return sum;
+	}
+	
+	public int multiply(int a, int b)
+	{
+		return a*b;
+	}
+}
