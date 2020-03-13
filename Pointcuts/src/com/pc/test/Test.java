083b7edf22c7b0a421ec1bae619f7e202c33abe8
Admin <Admin@ADMINRG-PK096SO>
2020-03-13 10:18:14 +0530

    Created ProxyFactory for advicing aspect over the target class

---
+++ Pointcuts/src/com/pc/test/Test.java
@@ -0,0 +1,20 @@
+package com.pc.test;
+
+import org.springframework.aop.framework.ProxyFactory;
+
+import com.pc.aspect.LoggingAdvice;
+import com.pc.beans.Calculator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ProxyFactory pf = new ProxyFactory();
+		pf.setTarget(new Calculator());
+		pf.addAdvice(new LoggingAdvice());
+		
+		Calculator proxy = (Calculator)pf.getProxy();
+		int sum = proxy.add(10, 20);
+		System.out.println("Addition is:"+sum);
+	}
+
+}
